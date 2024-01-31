79f0845b46937fff7aaf887744c27542f4d675d9
Wa101001 <whammouda47@gmail.Com>
2024-01-31 12:21:07 +0100

    tp 2 en cours...

--- tp2/stemmer.py
+++ tp2/stemmer.py
@@ -2,10 +2,8 @@ from nltk.stem import SnowballStemmer
 from statistics import preprocess_text
 
 def stem_text(data):
-    # Initialize SnowballStemmer for French
     stemmer_snowball = SnowballStemmer("french")
 
-    # Preprocess and stem the titles
     tokenized_titles_with_stemming = [stemmer_snowball.stem(preprocess_text(item['title'])) for item in data]
 
     return tokenized_titles_with_stemming
